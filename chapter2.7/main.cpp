d8fd7802d99639398641a55e7deeb0b4dbfad6b6
Md Yousuf Ali <aliyousuuf@gmail.com>
2021-03-20 20:00:41 +0600

    Update main.cpp

--- chapter2.7/main.cpp
+++ chapter2.7/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::endl;
 
+int getInteger(); //Forward declaration
+
+int main()
+{
+    int x{ getInteger()};
+    int y{ getInteger()};
+
+    std::cout << x << " + " << y << " is " << x + y << '\n';
+    return 0;
+}
