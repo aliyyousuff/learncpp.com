0bdfa2abd8f40d9adb37aca3f6c90a47709ad2b1
Md Yousuf Ali <aliyousuuf@gmail.com>
2021-03-23 09:03:27 +0600

    Add files via upload

---
+++ chapter_02_quiz/question03/io.cpp
@@ -0,0 +1,15 @@
+#include "io.h"
+#include <iostream>
+
+int readnumber()
+{
+    std::cout << "Enter an integer: ";
+    int x{ };
+    std::cin >> x;
+    return x;
+}
+
+void writeAnswer(int y)
+{
+    std::cout << "Sum of the both numbers is " << y << '\n';
+}
---
+++ chapter_02_quiz/question03/io.h
@@ -0,0 +1,8 @@
+#ifndef IO_H
+#define IO_H
+
+int readnumber();
+
+void writeAnswer(int y);
+
+#endif 
