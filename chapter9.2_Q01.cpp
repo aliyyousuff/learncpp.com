8978c2e0e36eca11b05a85599264fca5becf010b
Md Yousuf Ali <aliyousuuf@gmail.com>
2021-04-07 16:03:53 +0600

    Add files via upload

---
+++ chapter9.2_Q01.cpp
@@ -0,0 +1,22 @@
+/*
+Question #1
+
+1) Declare an array to hold the high temperature (to the nearest tenth of a degree) for each day of a year (assume 365 days in a year). 
+Initialize the array with a value of 0.0 for each day.
+
+*/
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <algorithm>
+
+
+
+int main()
+{
+    double temperature[100] { };
+
+    std::cout << temperature[0] << '\n';
+    return 0;
+}
