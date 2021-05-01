5708fddc763abe063b50d6d0cbdaba05ae058eea
Md Yousuf Ali <aliyousuuf@gmail.com>
2021-05-01 14:17:20 +0600

    Create Vector3d.cpp

---
+++ chapter11_15/Vector3d.cpp
@@ -0,0 +1,15 @@
+#include "Point3d.h"
+#include "Vector3d.h"
+
+#include <iostream>
+
+Vector3d::Vector3d(double x, double y, double z)
+    : m_x{ x }, m_y{ y }, m_z{ z }
+    {
+
+    }
+
+void Vector3d::print() const 
+{
+    std::cout << "Point(" << m_x << " , " << m_y << " , " << m_z << ")\n";
+}
