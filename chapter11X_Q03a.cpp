9d87517448c8ced2d8571832d26f4be0c8c83b59
Md Yousuf Ali <aliyousuuf@gmail.com>
2021-05-01 15:19:41 +0600

    Add files via upload

---
+++ chapter11X_Q03a.cpp
@@ -0,0 +1,41 @@
+/*
+  a) First, let’s create an enumeration of monster types named MonsterType. Include the following monster types: Dragon, 
+  Goblin, Ogre, Orc, Skeleton, Troll, Vampire, and Zombie. Add an additional max_monster_types enum so we can count how 
+  many enumerators there are.
+
+*/
+
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <array>
+#include <vector>
+#include <string_view>
+#include <functional>
+#include <cmath>
+#include <random>
+#include <cassert>
+#include <cstdint>
+
+enum class Monstertype
+{
+    DRAGON,
+    GOBLIN,
+    OGRE,
+    ORC,
+    SKELETAN,
+    TROLL,
+    ZOMBIE,
+    MAX_MONSTER_TYPES
+};
+
+int main()
+{
+    
+
+    return 0;
+}
