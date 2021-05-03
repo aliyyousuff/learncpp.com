b4042e4735e59f595235f445c22584033e03ed05
Md Yousuf Ali <aliyousuuf@gmail.com>
2021-05-03 19:06:46 +0600

    Add files via upload

---
+++ chapter13.9_Q01a.cpp
@@ -0,0 +1,38 @@
+/*
+
+A map is a class that stores elements as a key-value pair. The key must be unique, and is used to access the associated pair. 
+In this quiz, we’re going to write an application that lets us assign grades to students by name, using a simple map class. 
+The student’s name will be the key, and the grade (as a char) will be the value.
+a) First, write a struct named StudentGrade that contains the student’s name (as a std::string) and grade (as a char).
+*/
+
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <array>
+#include <vector>
+#include <string_view>
+#include <functional>
+#include <cmath>
+#include <random>
+#include <cassert>
+#include <cstdint>
+
+
+struct StudentGrade
+{
+    std::string student_name{ };
+    char grade{ };
+};
+
+
+int main()
+{
+
+ 
+  return 0;
+}
