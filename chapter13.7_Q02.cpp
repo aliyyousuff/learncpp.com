fbc36ce7c36b88a6769fc9b1984487ea1428f60b
Md Yousuf Ali <aliyousuuf@gmail.com>
2021-05-03 15:46:58 +0600

    Add files via upload

---
+++ chapter13.7_Q02.cpp
@@ -0,0 +1,86 @@
+/*
+
+2) Add an overloaded operator<< and operator< to the Car class at the top of the lesson so that the following program compiles:
+
+*/
+
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <array>
+#include <vector>
+#include <string_view>
+#include <functional>
+#include <cmath>
+#include <random>
+#include <cassert>
+#include <cstdint>
+
+class Car
+{
+    private:
+        std::string m_make{ };
+        std::string m_model{ };
+    
+    public:
+        Car( const std::string &make, const std::string &model )
+        : m_make{ make}, m_model{ model }
+        {
+
+        }
+
+        friend bool operator== (const Car &c1, const Car &c2);
+        friend bool operator!= (const Car &c1, const Car &c2);
+        friend bool operator< (const Car &c1, const Car &c2);
+        friend std::ostream& operator<< (std::ostream &out, const Car &c);
+
+};
+
+bool operator== (const Car &c1, const Car &c2)
+{
+    return (c1.m_make == c2.m_make && c1.m_model == c2.m_model);
+}
+
+bool operator!= (const Car &c1, const Car &c2)
+{
+    return !(c1 == c2);
+}
+
+bool operator< (const Car &c1, const Car &c2)
+{
+    if (c1.m_make == c2.m_make)
+    {
+        return c2.m_model < c2.m_model;
+    }
+    else 
+    {
+        return c1.m_make < c2.m_make;
+    }
+}
+
+std::ostream& operator<<(std::ostream &out, const Car &c)
+{
+    out << "(" << c.m_make << ", " << c.m_model << ")";
+    return out;
+}
+
+int main()
+{
+  std::vector<Car> cars{
+    { "Toyota", "Corolla" },
+    { "Honda", "Accord" },
+    { "Toyota", "Camry" },
+    { "Honda", "Civic" }
+  };
+ 
+  std::sort(cars.begin(), cars.end()); // requires an overloaded operator<
+ 
+  for (const auto& car : cars)
+    std::cout << car << '\n'; // requires an overloaded operator<<
+ 
+  return 0;
+}
